ee8be7b0fb9df77b05bc9a4bc9197dbdcd88f7b4
pavelmihai96 <pavelmihailungu@gmail.com>
2025-05-13 18:44:26 +0300

    Added spring boot project

---
+++ src/test/java/code/PriceComparator_Market/PriceComparatorMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package code.PriceComparator_Market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceComparatorMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
